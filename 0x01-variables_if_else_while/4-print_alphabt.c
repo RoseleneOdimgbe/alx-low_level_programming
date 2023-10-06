29ace823e1fd891e197e31ce5ec591d95357160d
RoseleneOdimgbe <babymesa98@gmail.com>
2023-10-06 16:15:05 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+char r;
+
+for (r = 'a'; r <= 'z'; r++)
+
+{
+if (r != 'e' && r != 'q')
+putchar(r);
+}
+
+putchar('\n');
+
+return (0);
+}
