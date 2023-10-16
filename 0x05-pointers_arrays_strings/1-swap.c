d52e1f79224a6444e211b0d683ab499aada44e00
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-16 15:21:01 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * swap_int - the function name
+  * @a: this is the first parameter
+  * @b: this is the second parameter
+*/
+
+void swap_int(int *a, int *b)
+
+{
+
+int r;
+r = *a;
+*a = *b;
+*b = r;
+
+}
