0e1310f6c6306156dddd214c90661fc499569b50
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-16 18:22:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * rev_string - the function name
+  * @s: this is the function parameter
+*/
+
+void rev_string(char *s)
+
+{
+
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+for (i = 0; i < count / 2; i++)
+{
+char w;
+w = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = w;
+}
+
+}
