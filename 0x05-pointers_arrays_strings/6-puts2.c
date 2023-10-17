bed671179a3f4223a8b4b67462787651de6929fa
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-16 18:36:39 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * puts2 - the function name
+  * @str: this is the function parameter
+*/
+
+void puts2(char *str)
+
+{
+
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (i % 2 == 0)
+		_putchar(str[i]);
+}
+	_putchar('\n');
+
+}
