3d2114a16637932c1241ffc91455f51baca0d926
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-16 18:03:12 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - the function name
+  * @s: this is the function parameter
+*/
+
+void print_rev(char *s)
+
+{
+
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+
+for (i = count - 1; i >= 0; i--)
+{
+	_putchar (s[i]);
+}
+	_putchar ('\n');
+
+}
