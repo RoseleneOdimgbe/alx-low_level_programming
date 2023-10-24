525ab54c870f8744b4e7d53d65a8f3e0f9503144
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-23 17:32:49 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_chessboard - the main function
+  *
+  * @a: the declared function parameter
+  *
+  * Return: Always 0.
+  */
+
+void print_chessboard(char (*a)[8])
+
+{
+
+	int k;
+	int r;
+
+	for (k = 0; k < 8; k++)
+	{
+	for (r = 0; r < 8; r++)
+	_putchar(a[k][r]);
+	_putchar('\n');
+	}
+
+}
