a8f6ed6372600fdd935f9ce890602c5815b4deec
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-23 17:44:07 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * set_string - the main function
+  *
+  * @s: the first function parameter
+  *
+  * @to: the second function parameter
+  *
+  * Return: Always 0
+  */
+
+void set_string(char **s, char *to)
+
+{
+
+	*s = to;
+
+}
