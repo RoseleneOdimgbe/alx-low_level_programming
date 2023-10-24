27fd28706d2d8f7c153029208b13cc417f9ae803
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-10-23 17:23:06 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strpbrk - the main function of the prototype
+  *
+  * @s: the first function parameter
+  *
+  * @accept: the second function parameter
+  *
+  * Return: Always 0.
+  */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+
+	int k, j;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[k] == accept[j])
+	return (s + k);
+	}
+	}
+	return (0);
+
+}
