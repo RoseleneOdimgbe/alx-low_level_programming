7dca9abbde667c6392f785d7cc760637757cd7d2
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-11-21 12:26:46 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * listint_len - this returns the number of elements in a linked lists
+ * @h: this is the linked list of type listint_t to tranverse
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+
+}
